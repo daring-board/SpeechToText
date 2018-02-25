c189ec83fc15bb5fc4edd92cdfc8d7031f0006b8
daring-board <mahotox101500@gmail.com>
2018-02-25 18:18:15 +0900

    This is first commit

---
+++ app/src/main/java/com/example/recoder/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.recoder;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private static String TAG = "Sample";
+    private SpeechRecognizer mRecognizer;
+    private RecognitionListener mRecognitionListener;
+    private ArrayList<String> sents;
+    // 音声合成用
+    private TextToSpeech tts = null;
+
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sents = new ArrayList<String>();
+        tts = new TextToSpeech(this, this);
+
+
+        mRecognitionListener = new RecognitionListener() {
+            @Override
+            public void onError(int error) {
+                if ((error == SpeechRecognizer.ERROR_NO_MATCH) ||
+                        (error == SpeechRecognizer.ERROR_SPEECH_TIMEOUT)) {
+                    startSpeechRecognition();
+                    return;
+                }
+                Log.d(TAG, "Recognition Error: " + error);
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> values = results
+                        .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                String val = values.get(0);
+                Log.d(TAG, "認識結果: " + val);
+                sents.add(val);
+                TextView textView = findViewById(R.id.textView2);
+                String lines = constractLines();
+                textView.setText(lines);
+                startSpeechRecognition();
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+            }
+
+            @Override
+            public void onBufferReceived(byte[] arg0) {
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+            }
+
+            @Override
+            public void onEvent(int arg0, Bundle arg1) {
+            }
+
+            @Override
+            public void onPartialResults(Bundle arg0) {
+            }
+
+            @Override
+            public void onReadyForSpeech(Bundle arg0) {
+            }
+
+            @Override
+            public void onRmsChanged(float arg0) {
+            }
+        };
+        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+    }
+
+    private String constractLines(){
+        String lines = "";
+        for(int i=0;i<sents.size();i++){
+            lines += (sents.get(i) + "\n");
+        }
+        return lines;
+    }
+
+    private void startSpeechRecognition() {
+        // Need to destroy a recognizer to consecutive recognition?
+        if (mRecognizer != null) {
+            mRecognizer.destroy();
+        }
+
+        // Create a recognizer.
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        mRecognizer.setRecognitionListener(mRecognitionListener);
+
+        // Start recognition.
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mRecognizer.startListening(intent);
+    }
+
+    public void start(View view){
+        startSpeechRecognition();
+    }
+
+    public void stop(View view){
+        if (mRecognizer != null) {
+            mRecognizer.destroy();
+        }
+    }
+
+    public void speech(View view){
+        // 音声合成して発音
+        if(tts.isSpeaking()) {
+            tts.stop();
+        }
+
+        for(int i = 0;i < sents.size(); i++){
+            tts.speak(sents.get(i), TextToSpeech.QUEUE_FLUSH, null);
+            while(tts.isSpeaking()) {
+                try {
+                    Thread.sleep(2000);
+                }catch (Exception e){
+                    Log.d(TAG, "Speech");
+                }
+            }
+        }
+    }
+
+    public void clear(View view){
+        String path = Environment.getExternalStorageDirectory().getPath() + "/file" + getNowDate() + ".txt";
+        Log.d(TAG, "出力先: " + path);
+        try{
+            String str = constractLines();
+            Log.d(TAG, getPackageName());
+            FileOutputStream out = new FileOutputStream( new File(path), true );
+            out.write(str.getBytes());
+            out.close();
+        }catch( IOException e ){
+            e.printStackTrace();
+        }
+        sents.clear();
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText("Empty");
+    }
+
+    private String getNowDate(){
+        final DateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        final Date date = new Date(System.currentTimeMillis());
+        return df.format(date);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if(status == TextToSpeech.SUCCESS) {
+            // 音声合成の設定を行う
+
+            float pitch = 1.0f; // 音の高低
+            float rate = 1.0f; // 話すスピード
+            Locale locale = Locale.JAPAN; // 対象言語のロケール
+            // ※ロケールの一覧表
+            //   http://docs.oracle.com/javase/jp/1.5.0/api/java/util/Locale.html
+
+            tts.setPitch(pitch);
+            tts.setSpeechRate(rate);
+            tts.setLanguage(locale);
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if( tts != null )
+        {
+            // 破棄
+            tts.shutdown();
+        }
+    }
+
+
+
+//    /**
+//     * A native method that is implemented by the 'native-lib' native library,
+//     * which is packaged with this application.
+//     */
+//    public native String stringFromJNI();
+
+}
+
+
+
+
